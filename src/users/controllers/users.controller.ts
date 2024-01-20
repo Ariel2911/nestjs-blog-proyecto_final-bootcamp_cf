f844906728e7d9b46547e560603849b8af3dcc7d
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-20 13:23:08 -0300

    Agrega createUser() en UsersSevice y createUser() en UsersController

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { UsersService } from '../services/users.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Crea un usuario' })
+  @ApiResponse({
+    status: 201,
+    description: 'Created user',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Are missing or the type of data is incorrect',
+  })
+  createUser() {
+    return this.usersService.createUser();
+  }
+}
