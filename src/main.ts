e3173cee3fb1343a06caa3d8b75c4a52b4dc945a
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-20 13:55:37 -0300

    Agrega y configura variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
 
   const config = new DocumentBuilder()
     .setTitle('Cody blog API')
@@ -14,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('docs', app, document);
-  await app.listen(3000);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
