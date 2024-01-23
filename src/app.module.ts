750f9ed28c484d65e247ea4a497cd6843dc3e17d
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-23 09:50:39 -0300

    Instala las dependencias de mongoose y declara la conección con la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: `.${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.DB_URI),
     UsersModule,
   ],
 })
