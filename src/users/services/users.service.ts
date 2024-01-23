c35e20182baa5a09fa83fccbfa7ac9ebf75a31c8
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-23 10:26:57 -0300

    Declara userModel y modifica createUser()

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from '../schemas/usersSchema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from '../dto/create-user';
 
 @Injectable()
 export class UsersService {
-  createUser() {
-    return 'Created user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+
+    return createdUser.save();
   }
 }
