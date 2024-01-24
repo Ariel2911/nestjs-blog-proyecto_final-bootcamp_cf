fa3184d5bd7a34eeeec4a2d5ddb27661d2cbc630
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-23 22:06:15 -0300

    Agrega comentarios en createUser

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -12,9 +12,16 @@ import { CreateUserDto } from '../dto/create-user.dto';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
+  /**
+   *
+   * @param createUserDto
+   * @returns Promise<User>
+   */
+
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     try {
       const createdUser = await this.userModel.create(createUserDto);
+
       return createdUser;
     } catch (error) {
       if (error.code === 11000) {
@@ -22,7 +29,9 @@ export class UsersService {
           `The ${Object.keys(error.keyPattern)} already exist`,
         );
       }
+
       console.log(error);
+
       throw new InternalServerErrorException("Can't create user");
     }
   }
