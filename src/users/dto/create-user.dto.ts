ef9aeaf391a4c8bbb30c8ee4361d572bb1366b09
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-23 21:50:07 -0300

    Agrega datoss en la documentación de swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsBoolean,
   IsEmail,
@@ -8,21 +9,39 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'Cody',
+    minLength: 3,
+    maxLength: 200,
+    description: 'User name',
+  })
   @IsNotEmpty()
   @IsString()
   @Length(3, 200)
   readonly name: string;
 
+  @ApiProperty({
+    example: 'password',
+    minLength: 8,
+    maxLength: 16,
+    description: 'User password',
+  })
   @IsNotEmpty()
   @IsString()
-  @Length(8, 20)
+  @Length(8, 16)
   readonly password: string;
 
+  @ApiProperty({ example: 'Cody@test.com', description: 'User email' })
   @IsNotEmpty()
   @IsString()
   @IsEmail()
   readonly email: string;
 
+  @ApiProperty({
+    example: 'true',
+    default: false,
+    description: 'Optional value',
+  })
   @IsOptional()
   @IsBoolean()
   readonly isAdmin?: boolean;
