b156278d890662a0b10a46077c72a17c8c1e5783
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-25 18:29:55 -0300

    Documenta /api/users/login

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,13 +1,26 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { LoginAuthDto } from '../dto/login.auth.dto';
 
+@ApiTags('Login')
 @Controller('users')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @ApiOperation({ summary: 'Login user' })
+  @ApiBody({ type: LoginAuthDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Logged in user. Returns access_token',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. The data provided does not belong to any user',
+  })
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
