74d7f751f87a7d012a05daa67fe80f3a3f2f3869
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-25 14:23:56 -0300

    Agrega ruta Post-login

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from '../services/auth.service';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('users')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return req.user;
+  }
+}
