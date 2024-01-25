c7221b93d61541a05a5052b58304edf79f9b6e68
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-25 12:52:54 -0300

    Agrega findUser() y validateUser()

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/users/schemas/usersSchema';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  async findUser(email: string): Promise<User> {
+    return this.userModel.findOne({ email });
+  }
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.findUser(email);
+
+    if (user && user.email === email && user.password === password) {
+      return { name: user.name };
+    }
+    return null;
+  }
+}
