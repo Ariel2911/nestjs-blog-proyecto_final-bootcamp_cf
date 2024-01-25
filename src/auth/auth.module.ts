eeb9e88e7cbfdfd4dfd5cbf9796a0e2a40a6c374
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-25 16:10:40 -0300

    Agrega en Imports el Jwtmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,15 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/users/schemas/usersSchema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     PassportModule,
+    JwtModule.register({
+      secret: `${process.env.JWT_SECRET}`,
+      signOptions: { expiresIn: '200s' },
+    }),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [AuthController],
