ce9a965d4bcb9ac90bbadec7b42676d9a52be37e
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-26 18:54:57 -0300

    Cambia la configuración de JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,21 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { User, UserSchema } from 'src/users/schemas/usersSchema';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
-import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/users/schemas/usersSchema';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { jwtConfig } from 'src/config/jwt.config';
 
 @Module({
   imports: [
     PassportModule,
-    JwtModule.register({
-      secret: `${process.env.JWT_SECRET}`,
-      signOptions: { expiresIn: '200s' },
-    }),
+    JwtModule.registerAsync(jwtConfig),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
