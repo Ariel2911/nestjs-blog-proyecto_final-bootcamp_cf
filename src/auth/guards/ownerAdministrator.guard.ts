d22680d258e20e7cf7909948a68bc68f09d2336f
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-29 18:26:09 -0300

    Declara ownerAdministrator.guard

---
+++ src/auth/guards/ownerAdministrator.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AdminKey } from '../decorators/isAdmin.decorator';
+import { OwnerKey } from '../decorators/isOwner.decorator';
+
+@Injectable()
+export class OwnerOrAdministratorOnlyGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requireAdmin = this.reflector.getAllAndOverride<boolean>(AdminKey, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    const requireOwner = this.reflector.getAllAndOverride<boolean>(OwnerKey, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    const { user } = context.switchToHttp().getRequest();
+    const { params } = context.switchToHttp().getRequest();
+
+    if (requireAdmin === true && user.isAdmin === true) return true;
+    if (requireOwner === true && user._id === params.id) return true;
+
+    return false;
+  }
+}
