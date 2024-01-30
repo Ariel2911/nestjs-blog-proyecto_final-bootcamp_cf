f24da0a8da257eb28bdbff6c1e3718f4dae30825
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-30 20:44:19 -0300

    Declara postsSchema

---
+++ src/posts/schemas/postsSchema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type PostDocument = HydratedDocument<Post>;
+
+@Schema()
+export class Post {
+  @Prop({
+    type: String,
+    required: true,
+    min: [3, 'Must be at least 3 characters'],
+    max: [255, 'Must have a maximum of 255 characters'],
+  })
+  title: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  author: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  content: string;
+
+  @Prop({
+    type: [String],
+  })
+  categories: string[];
+}
+
+export const PostSchema = SchemaFactory.createForClass(Post);
