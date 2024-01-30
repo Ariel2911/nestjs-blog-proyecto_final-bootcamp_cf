63142e265cc01bfddbd2ac792fed3e220b23c919
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-30 20:47:33 -0300

    Agrega la importación de PostSchema

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './services/posts.service';
 import { PostsController } from './controllers/posts.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Post, PostSchema } from './schemas/postsSchema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+  ],
   providers: [PostsService],
   controllers: [PostsController],
 })
