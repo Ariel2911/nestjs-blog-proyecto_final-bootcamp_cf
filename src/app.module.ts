40cfa8d35530fee127ce54b2320998e20cdb75c7
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-29 22:14:51 -0300

    Declara el modulo Posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     MongooseModule.forRoot(process.env.DB_URI),
     UsersModule,
     AuthModule,
+    PostsModule,
   ],
 })
 export class AppModule {}
