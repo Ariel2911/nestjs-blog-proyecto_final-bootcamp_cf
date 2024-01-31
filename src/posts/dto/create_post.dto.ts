e3033b1bfe595384e4be859e9459c838b07cc585
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-30 21:13:11 -0300

    Agrega validaciones en CreatePostDto

--- src/posts/dto/create_post.dto.ts
+++ src/posts/dto/create_post.dto.ts
@@ -1,6 +1,20 @@
+import { IsArray, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 255)
   readonly title: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly author: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly content: string;
+
+  @IsNotEmpty()
+  @IsArray({ each: true })
   readonly categories: string[];
 }
