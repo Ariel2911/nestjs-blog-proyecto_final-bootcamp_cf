5c5cda38033f7c6a15f1992230ae13e82555d63f
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-30 22:17:11 -0300

    Declara la ruta  api/post con Post

--- src/posts/controllers/posts.controller.ts
+++ src/posts/controllers/posts.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { PostsService } from '../services/posts.service';
+import { CreatePostDto } from '../dto/create_post.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  createPost(
+    @Body() createPostDto: CreatePostDto,
+    @Req() req: Request & { user: { name: string } },
+  ): Promise<any> {
+    return this.postsService.createPost(createPostDto, req);
+  }
+}
