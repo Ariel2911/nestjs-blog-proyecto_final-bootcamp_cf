b6d80975c8faf2527e509360c9a638fefa76baa0
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-30 21:36:18 -0300

    Declara createPost

--- src/posts/services/posts.service.ts
+++ src/posts/services/posts.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post } from '../schemas/postsSchema';
+import { Model } from 'mongoose';
+import { CreatePostDto } from '../dto/create_post.dto';
+import { ErrorManager } from 'src/utils/error.manager';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(@InjectModel(Post.name) private postModel: Model<Post>) {}
+
+  /**
+   *
+   * @param createPostDto
+   * @returns Promise<Post>
+   */
+  async createPost(createPostDto: CreatePostDto): Promise<Post> {
+    try {
+      const createdPost = this.postModel.create(createPostDto);
+
+      return createdPost;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+}
